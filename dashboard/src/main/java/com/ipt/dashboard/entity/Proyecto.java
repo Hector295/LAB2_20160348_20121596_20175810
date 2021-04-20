e4b4ef755ad554c417e728be5934b4cd9ebe37c4
Fabrizio <a20160348@pucp.edu.pe>
2021-04-19 20:10:38 -0500

    Proyecto

---
+++ dashboard/src/main/java/com/ipt/dashboard/entity/Proyecto.java
@@ -0,0 +1,44 @@
+package com.ipt.dashboard.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "proyectos")
+public class Proyecto {
+
+    @Id
+    @Column(nullable = false)
+    private int proyectoid;
+    @Column
+    private String proyectonombre;
+    @Column(nullable = false)
+    private String usuariocorreo;
+
+    public int getProyectoid() {
+        return proyectoid;
+    }
+
+    public void setProyectoid(int proyectoid) {
+        this.proyectoid = proyectoid;
+    }
+
+    public String getProyectonombre() {
+        return proyectonombre;
+    }
+
+    public void setProyectonombre(String proyectonombre) {
+        this.proyectonombre = proyectonombre;
+    }
+
+    public String getUsuariocorreo() {
+        return usuariocorreo;
+    }
+
+    public void setUsuariocorreo(String usuariocorreo) {
+        this.usuariocorreo = usuariocorreo;
+    }
+}
