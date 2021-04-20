665457ed96b7c705a8f81e603fc2f0f398a89642
sarita atanacio <atanacio.sarita@pucp.pe>
2021-04-19 23:30:09 -0500

    CRUD AREAS + VISTAS final

--- dashboard/src/main/java/com/ipt/dashboard/repository/UsuarioRepository.java
+++ dashboard/src/main/java/com/ipt/dashboard/repository/UsuarioRepository.java
@@ -1,7 +1,15 @@
 package com.ipt.dashboard.repository;
 
+import com.ipt.dashboard.entity.Areas;
 import com.ipt.dashboard.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,String> {
+
+    @Query(value = "select * from usuarios where idarea like %?1%",nativeQuery = true)
+    List<Usuario> filtrarUsuariosPorId(int id);
+
 }
