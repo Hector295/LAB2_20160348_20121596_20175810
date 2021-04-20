047b3c313096acf84d6dd8c8b47b404e05d75370
Hector <hector.ventura@pucp.edu.pe>
2021-04-19 20:14:27 -0500

    Usuario parte1

--- dashboard/src/main/java/com/ipt/dashboard/controller/UsuarioController.java
+++ dashboard/src/main/java/com/ipt/dashboard/controller/UsuarioController.java
@@ -1,13 +1,66 @@
 package com.ipt.dashboard.controller;
 
+import com.ipt.dashboard.entity.Usuario;
+import com.ipt.dashboard.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
-    @GetMapping("")
+    @GetMapping({"","/list"})
     public String listarUsaurios(Model model){
+        List<Usuario> lista = usuarioRepository.findAll();
+        model.addAttribute("listaUsuarios",lista);
+    return "usuario/listaUsuario";
+    }
+    @GetMapping("/nuevo")
+    public String nuevoUsuario() {
+        return "usuario/nuevoUsuario";
+    }
 
+    @PostMapping("/guardar")
+    public String guardarUsuario(Usuario usuario, RedirectAttributes attributes){
+        if(usuario.getCorreo()==null){
+            attributes.addFlashAttribute("mensaje","Usuario creado exitosamente");
+        }else {
+            attributes.addFlashAttribute("mensaje","Usuario editado exitosamente");
+        }
+        usuarioRepository.save(usuario);
+        return "redirect: /usuario/list";
+    }
+    @GetMapping("/editar")
+    public String editarUsuario(Model model,
+                                @RequestParam("correo") String correo){
+        Optional<Usuario> optionalUsuario=usuarioRepository.findById(correo);
+        if(optionalUsuario.isPresent()){
+            Usuario usuario = optionalUsuario.get();
+            model.addAttribute("usuario",usuario);
+            return "usuario/editarUsuario";
+        }else {
+            return "redirect:/usuario";
+        }
+    }
+    @GetMapping("/eliminar")
+    public String eliminarUsuario(@RequestParam("correo") String correo,
+                                  RedirectAttributes attributes){
+        Optional<Usuario> optionalUsuario=usuarioRepository.findById(correo);
+        if(optionalUsuario.isPresent()){
+            usuarioRepository.deleteById(correo);
+            attributes.addFlashAttribute("mensaje","Usuario borrado exitosamente");
+        }
+        return "redirect:/usuario";
     }
 }
--- dashboard/src/main/java/com/ipt/dashboard/entity/Usuario.java
+++ dashboard/src/main/java/com/ipt/dashboard/entity/Usuario.java
@@ -1,5 +1,47 @@
 package com.ipt.dashboard.entity;
 
-public class Usuario {
+import javax.persistence.*;
 
+@Entity
+@Table(name="usuarios")
+public class Usuario{
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(int idarea) {
+        this.idarea = idarea;
+    }
+
+    @Id
+    @Column(nullable = false)
+    private String correo;
+    private String nombres;
+    private String apellidos;
+    @Column(nullable = false)
+    private int idarea;
 }
